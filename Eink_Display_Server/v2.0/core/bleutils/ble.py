13b39ebd61e413f98dc187417079302299004f6c
huytp2001 <dqhuy.dev2001@gmail.com>
2022-07-17 10:05:19 +0700

    add ble which have basic function include parallel

--- Eink_Display_Server/v2.0/core/bleutils/ble.py
+++ Eink_Display_Server/v2.0/core/bleutils/ble.py
@@ -1,97 +1,72 @@
 import pexpect
 import time
-import os
 
+# Basic function include parallel -> Done
+# Error Handling -> Working
+# Functional -> In queue
 
-class BLE_Error(Exception):
-    pass
+macs = []
+blue = pexpect.spawn("bluetoothctl", echo=False)
+blue.sendline("power on")
+blue.expect("succeeded", timeout=3)
+blue.sendline("scan on")
+blue.expect("bluetooth", timeout=3)
+time.sleep(2)
+blue.sendline("scan off")
+blue.expect("bluetooth", timeout=3)
+blue.sendline("devices")
+blue.expect("bluetooth", timeout=3)
 
+raw_data = blue.before.split(b"\r\n")
+for data in raw_data:
 
-class BLE_Controller():
-    def __init__(self):
-        self.bluectl = pexpect.spawn("bluetoothctl", echo=False)
-        self.gatttool = pexpect.spawn("sudo gatttool -I", echo=False)
+    if b"JDY-23" in data:
+        split_data = (data.decode("utf-8")).split()
+        if len(split_data) == 4:
+            macs.append(split_data[2])
+        else:
+            macs.append(split_data[1])
 
-    def SentCommand(self, child: str, command: str, expect: str, timeout=3, attempts=5):
-        attempt = 0
-        success = False
-        while not success and attempt < attempts:
-            try:
-                if child == "bluectl":
-                    self.bluectl.sendline(command)
-                    self.bluectl.expect(expect, timeout=timeout)
-                if child == "gatttool":
-                    self.gatttool.sendline(command)
-                    self.gatttool.expect(expect, timeout=timeout)
-                success = True
-            except:
-                attempt += 1
-                if attempt >= attempts:
-                    raise BLE_Error("BLE_Error!!")
+dev_list = list()
+for idex,mac in enumerate(macs) :
+    p = pexpect.spawn("sudo gatttool -I")
+    print(f"Creating child process {idex}...")
+    p.expect("LE", timeout=3)
+    p.sendline(f"connect {mac}")
+    p.expect("successful", timeout=3)
+    print(f"Create child process {idex} success!")
+    dev_list.append({'mac':mac,'cli':p})
 
-    def Scan(self, scan_time: int):
-        # Return a list of Eink device
-        devs = []
+while 1:
+    data = input("Enter: ")
+    if data == "exit":
+        blue.sendline("exit")
+        blue.expect("$", timeout=3)
+        blue.close()
+        for idx, dev in enumerate(dev_list):
+            dev['cli'].sendline("disconnect")
+            dev['cli'].expect("descriptor", timeout=3)
+            dev['cli'].sendline("exit")
+            dev['cli'].expect("$", timeout=3)
+            dev['cli'].close()
+            print(f"Kill process {idx}")
+        break
 
-        self.SentCommand("bluectl", "power on", "succeeded")
-        self.SentCommand("bluectl", "scan on", "bluetooth")
-        time.sleep(scan_time)
-        self.SentCommand("bluectl", "scan off", "bluetooth")
-        self.SentCommand("bluectl", "devices", "bluetooth")
+    if data == "test":
+        for i in range(10):
+            print(f"Start round {i}")
+            for dev in dev_list:    
+                data = f"@s{i}|{i}@e"
+                print(f"Write {data} to {dev['mac']}...")
+                hex_data = "".join("{:02x}".format(ord(c)) for c in data)
+                dev['cli'].sendline(f"char-write-req 0x0000f {hex_data}")
+                dev['cli'].expect("successfully", timeout=3)
+                print(f"Write {data} to {dev['mac']} done")
+            print(f"End round {i}")
+            time.sleep(6)
 
-        raw_data = self.bluectl.before.split(b"\r\n")
-        for data in raw_data:
-            if b"JDY-23" in data:
-                split_data = (data.decode("utf-8")).split()
-                if len(split_data) == 4:
-                    devs.append({"mac": split_data[2], "name": split_data[3]})
-                else:
-                    devs.append({"mac": split_data[1], "name": split_data[2]})
-        return devs
-
-    def SentData(self, mac: str, data: str, attempts: int):
-        # Send data to exactly one Eink specficy by mac address
-
-        self.SentCommand("gatttool", f"connect {mac}", "successful")
-        hex_data = "".join("{:02x}".format(ord(c)) for c in data)
-        self.SentCommand(
-            "gatttool", f"char-write-req 0x0000f {hex_data}", "successfully")
-        self.SentCommand("gatttool", "disconnect", "descriptor")
-
-
-if __name__ == "__main__":
-    devs = []
-    ble = BLE_Controller()
-    while 1:
-        command = input("Enter: ")
-        command_list = command.split()
-        if command_list[0] == "scan":
-            devs = ble.Scan(2)
-            for dev in devs:
-                print(dev['mac'], dev['name'])
-
-        if command_list[0] == "send":
-            ble.SentData(command_list[1], command_list[2], 5)
-
-        if command_list[0] == "refresh":
-            devs = ble.Scan(2)
-            for dev in devs:
-                ble.SentData(dev['mac'], "@srefresh@e", 5)
-
-        if command_list[0] == "send_all":
-            devs = ble.Scan(2)
-            for dev in devs:
-                ble.SentData(dev['mac'], command_list[1], 5)
-
-        if command_list[0] == "exit":
-            break
-
-        if command_list[0] == "clear":
-            os.system("clear")
-
-        if command_list[0] == "help":
-            print("scan:              Scan for nearby Eink device\nsend {mac} {data}: Send {data} to Eink device with {mac} address\nrefresh:           Refresh all Eink devices\nsend_all {data}:   Send {data} to all Eink devices nearby\nclear:             Clear screen\nexit:              Exit this CLI\nhelp:              Show this help")
-
-
-
-
+    else:
+        for dev in dev_list:
+            hex_data = "".join("{:02x}".format(ord(c)) for c in data)
+            dev['cli'].sendline(f"char-write-req 0x0000f {hex_data}")
+            dev['cli'].expect("successfully", timeout=3)
